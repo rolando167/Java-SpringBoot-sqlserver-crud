782456b2ac21d953fc426788d7e775f5e571b61f
Rolando HR <rolando167@hotmail.com>
2023-03-05 17:41:33 -0500

    commit

--- src/main/java/com/crud/cards/repository/CardRepositoryImp.java
+++ src/main/java/com/crud/cards/repository/CardRepositoryImp.java
@@ -1,4 +1,5 @@
 package com.crud.cards.repository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -25,22 +26,35 @@ public class CardRepositoryImp implements ICardRepository{
 	@Override
 	public int save(Cards card) {
 		String SQL = "INSERT INTO spring.cards VALUES (?,?,?,?,?,?,?,?)";
-		return jdbcTemplate.update(SQL, new Object[] {card.getName(), card.getNumber(),
-				card.getType(), card.getCvv(), card.getStatus(), card.getCreated_by(),
-				card.getCreated_at()});
+		return jdbcTemplate.update(SQL, new Object[] {
+				card.getName(), 
+				card.getNumber(),
+				card.getType(),
+				card.getCvv(), 
+				card.getStatus(),
+				card.getCreated_by(),
+				LocalDateTime.now(), 
+				LocalDateTime.now()
+				});
 	}
 
 	@Override
 	public int update(Cards card) {
-		String SQL = "UPDATE spring.cards SET name=?, number=?, type=?, cvv=? WHERE id_card = ?";
-		return jdbcTemplate.update(SQL, new Object[] {card.getName(), card.getNumber(),
-				card.getType(), card.getCvv(), card.getId_card()});
+		String SQL = "UPDATE spring.cards SET name=?, number=?, type=?, cvv=? , updated_at=? WHERE id_card = ?";
+		return jdbcTemplate.update(SQL, new Object[] {
+				card.getName(), 
+				card.getNumber(),
+				card.getType(),
+				card.getCvv(),
+				LocalDateTime.now(),
+				card.getId_card()
+				});
 	}
 
 	@Override
 	public int deleteByid(int id) {
-		String SQL = "UPDATE spring.cards SET status = 0 WHERE id_card = ?";
-		return jdbcTemplate.update(SQL, new Object[] {id});
+		String SQL = "UPDATE spring.cards SET status = 0, updated_at=? WHERE id_card = ?";
+		return jdbcTemplate.update(SQL, new Object[] {LocalDateTime.now(), id});
 	}
  
 }
